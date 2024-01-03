9b075596ed676380369a594435d4885f4384a8f5
Ping <ping15512@gmail.com>
2024-01-03 14:22:48 +0700

    feat:add react context

--- client/src/pages/ViewProductPage.jsx
+++ client/src/pages/ViewProductPage.jsx
@@ -1,7 +1,10 @@
 import { useNavigate } from "react-router-dom";
+import { useContext } from "react";
+import { LevelContext } from "../contexts/LevelContext";
 
 function ViewProductPage() {
   const navigate = useNavigate();
+  const contextData = useContext(LevelContext);
   return (
     <div>
       <h1>View Product Page</h1>
@@ -11,7 +14,10 @@ function ViewProductPage() {
       </div>
 
       <div className="product-promotion-box">
-        <h2>คุณคือสมาชิกในระดับ (x) ดังนั้นคุณได้สิทธิพิเศษลด 50%</h2>
+        <h2>
+          คุณคือสมาชิกในระดับ {contextData.LevelState} ดังนั้นคุณได้สิทธิพิเศษลด
+          50%
+        </h2>
         <button onClick={() => alert("🥳🥳🥳🥳")}>กดรับสิทธิ</button>
       </div>
 
